52ee905ae0880d7442ccfa2d07737b556a990b52
pypasquao <paula.pasqualini@taqtile.com>
2017-05-04 08:54:10 -0300

    Level 5

--- AppBasico/AppBasico/ProductViewController.swift
+++ AppBasico/AppBasico/ProductViewController.swift
@@ -12,10 +12,13 @@ class ProductViewController: UIViewController {
 
     @IBOutlet weak var productImageView: UIImageView!
     @IBOutlet weak var productNameLabel: UILabel!
+    
+    var productName: String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        productNameLabel.text = "1937 Desk Phone"
+        productNameLabel.text = productName
         productImageView.image = UIImage(named: "phone-fullscreen3")
         
         
--- AppBasico/AppBasico/ProductsTableViewController.swift
+++ AppBasico/AppBasico/ProductsTableViewController.swift
@@ -9,9 +9,20 @@
 import UIKit
 
 class ProductsTableViewController: UITableViewController {
+    
+    var productNames: [String]?
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        productNames = ["Product 1", "Product 2", "Product 3", "Product 4"]
+    }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        if let pNames = productNames{
+            return pNames.count
+        }
+        return 0
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -20,10 +31,53 @@ class ProductsTableViewController: UITableViewController {
 //      if(cell.textLabel != nil){
 //          cell.textLabel!.text = "Hello friend."
 //      }
-        cell.textLabel?.text = "Hello friend."
+        
+        let productName = productNames?[indexPath.row]
+        
+        if let pName = productName{
+            cell.textLabel?.text = pName
+        }
+        
         cell.imageView?.image = UIImage(named: "image-cell1")
+    
         
         return cell
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?){
+        
+        if segue.identifier == "ShowProduct"{
+            
+            let productVC = segue.destination as? ProductViewController
+
+//          Primeira opcao
+//            if let cell = sender as? UITableViewCell{
+//                let indexPath = tableView.indexPath(for: cell)
+//                if let indexPath = indexPath{
+//                    let productName = productNames?[indexPath.row]
+//                    productVC?.productName = productName
+//                }
+//            }
+            
+//          Segunda opcao
+//            if let cell = sender as? UITableViewCell{
+//                if let indexPath = tableView.indexPath(for: cell){
+//                    productVC?.productName = productNames?[indexPath.row]
+//                }
+//            }
+            
+            guard let cell = sender as? UITableViewCell,
+                  let indexPath = tableView.indexPath(for: cell) else{
+                return
+            }
+            
+            productVC?.productName = productNames?[indexPath.row]
+            
+            
+            
+            
+        }
+        
     }
     
 }
